1d467f6af8e12aaa4dd56f553adb31e0fb4b64e2
Antonio-Romes <antonioromes1@hotmail.com>
2025-06-15 22:33:37 -0300

    Criação inicial do projeto Auto Peça Moto

---
+++ src/main/java/com/example/AutoPecaMoto/AutoPecaMotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AutoPecaMoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPecaMotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPecaMotoApplication.class, args);
+	}
+
+}

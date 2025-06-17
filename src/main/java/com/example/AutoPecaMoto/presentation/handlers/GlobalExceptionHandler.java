90b4d8350a8ddf1b0a86f7461956feda3ab47ce3
Antonio-Romes <antonioromes1@hotmail.com>
2025-06-17 17:21:57 -0300

    criar classe para capturar execption global

---
+++ src/main/java/com/example/AutoPecaMoto/presentation/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+
+package com.example.AutoPecaMoto.presentation.handlers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
